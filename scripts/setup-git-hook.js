1af2a137296f214ace852d426d7278edae27564e
Demons Light <jetaimefrc@gmail.com>
2019-12-19 22:50:19 +0700

    chore(workflow): add verify commit msg

---
+++ scripts/setup-git-hook.js
@@ -0,0 +1,10 @@
+const { test, ln, chmod } = require('shelljs')
+
+function installHooks() {
+  if (test('-e', '.git/hooks')) {
+    ln('-sf', '../../scripts/git-hooks/commit-msg', '.git/hooks/commit-msg')
+    chmod('+x', '.git/hooks/commit-msg')
+  }
+}
+
+installHooks()
